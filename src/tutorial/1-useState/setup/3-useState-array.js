b83b4f58bd15e59fe45edb1ba169a5f31b263cd6
Chhavi Arora <chhaviarora1401@gmail.com>
2022-01-11 00:21:57 +0530

    Functional Update Form

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -5,24 +5,21 @@ const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
   const removeItem = (id) => {
-
-    setPeople((oldPeople) => {
-      let newPeople = oldPeople.filter((person) => person.id !== id);
-      return newPeople;
-    });
-  };
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople)
+  }
   return (
     <div className="example">
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} style={{ marginBottom: '3rem' }}>
-            <h2>{name}</h2>
+          <div key={id} className='item'>
+            <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button className="btn" onClick={() => setPeople([])}>
+      <button className='btn' onClick={() => setPeople([])}>
         clear items
       </button>
     </div>
